245a154408e361be44153d35f197b457702c08dc
Olexander <rybkolhoz@gmail.com>
2023-11-13 19:48:00 +0200

    first

---
+++ src/main/java/salat/server/HttpServer.java
@@ -0,0 +1,12 @@
+package salat.server;
+
+/**
+ *
+ *  Driver Class for Http Server
+ */
+public class HttpServer {
+    public static void main (String[] args){
+        System.out.print(" Server starting... ");
+    }
+
+}
