01a1fa1e79982271444e924615d46890c85e6409
MolchanovAlexander <rybkolhoz@gmail.com>
2023-11-23 09:49:47 +0200

    newMeth1

---
+++ src/main/java/salat/server/core/HttpConnectionWorkerThread.java
@@ -0,0 +1,48 @@
+package salat.server.core;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class HttpConnectionWorkerThread extends  Thread {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ServerListnerThread.class);
+
+    private  Socket socket;
+    public  HttpConnectionWorkerThread(Socket socket) {
+        this.socket = socket;
+    }
+    @Override
+    public void run(){
+        try {
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            // todo we would read
+            String html = "<html><head><title>JAVA server</title></head><body ><div style=\"display:flex;align-items:center;height:300px; background-color:lightgrey;justify-content:center;\"><h1 style=\"color:red\">JAVA GUF RIP</h1></div></body></html>";
+            final String CRLF = "\n\r";
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +
+                            "Content-Length: " + html.getBytes().length + CRLF +
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+            // todo we would writing
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            LOGGER.info("Connect processing finished.");
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
